735441967fdc8c98e260c520255e3a78099e5045
jgvictores <jcgvicto@gmail.com>
2022-12-04 10:56:41 +0100

    init gymnasium_playground_gridworld

---
+++ gridworld/gymnasium_playground_gridworld/envs/grid_world.py
@@ -0,0 +1,25 @@
+import gymnasium as gym
+import numpy as np
+import pygame
+from gymnasium import spaces
+
+class GridWorldEnv(gym.Env):
+    metadata = {"render_modes": ["human", "rgb_array"]}
+
+    def __init__(self, render_mode=None):
+        pass
+
+    def _get_obs(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def step(self, action):
+        pass
+
+    def render(self):
+        pass
+
+    def close(self):
+        pass
