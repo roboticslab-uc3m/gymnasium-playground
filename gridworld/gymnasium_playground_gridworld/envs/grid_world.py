6a17730ffaf75db7aa14dd4d52783ed5ad7f4694
jgvictores <jcgvicto@gmail.com>
2022-12-05 16:41:13 +0100

    map, observation_space, action_space

--- gridworld/gymnasium_playground_gridworld/envs/grid_world.py
+++ gridworld/gymnasium_playground_gridworld/envs/grid_world.py
@@ -7,7 +7,21 @@ class GridWorldEnv(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"]}
 
     def __init__(self, render_mode=None):
-        pass
+        # Remember: X points down, Y points right, thus Z points outwards.
+        # hard-coded vars (begin)
+        inFileStr = 'map1.csv'
+        initX = 2
+        initY = 2
+        goalX = 7
+        goalY = 2
+        # hard-coded vars (end)
+        self.inFile = np.genfromtxt(inFileStr, delimiter=',')
+        self.inFile[goalX][goalY] = 3 # The goal (3) is fixed, so we paint it, but the robot (2) moves, so done at render().
+        nrow, ncol = self.inFile.shape
+        self.observation_space = spaces.Discrete(nrow * ncol)
+
+        # We have 4 actions, corresponding to "right", "up", "left", "down".
+        self.action_space = spaces.Discrete(4)
 
     def _get_obs(self):
         pass
