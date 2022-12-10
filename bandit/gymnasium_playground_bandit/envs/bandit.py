88db7a2368b66514f7407464075c3cc1aa4cd93e
jgvictores <jcgvicto@gmail.com>
2022-12-10 12:53:09 +0100

    init implement bandit
    - Heavy influence from https://github.com/magni84/gym_bandits/blob/2f8b123cbe5d2713b63ea679ec7e220b6857ce04/gym_bandits/envs/bandits_env.py

--- bandit/gymnasium_playground_bandit/envs/bandit.py
+++ bandit/gymnasium_playground_bandit/envs/bandit.py
@@ -2,27 +2,35 @@ import gymnasium as gym
 import numpy as np
 from gymnasium import spaces
 
-
 class BanditEnv(gym.Env):
     metadata = {"render_modes": ["human"]}
 
-    def __init__(self, render_mode=None):
-        pass
+    def __init__(self, render_mode=None, n_arms=10):
+        super(BanditEnv, self).__init__()
+        self.observation_space = spaces.Discrete(1)
+        self.observation = 0
+        self.action_space = spaces.Discrete(n_arms)
 
     def _get_obs(self):
-        pass
+        return self.observation
 
     def _get_info(self):
-        pass
+        return {
+            "optimal": self.optimal
+        }
 
     def reset(self, seed=None, options=None):
-        pass
+        self.means = np.random.randn(self.action_space.n)
+        self.optimal = np.argmax(self.means)
+        return self._get_obs(), self._get_info()
 
     def step(self, action):
-        pass
+        assert self.action_space.contains(action)
+        reward = np.random.randn(1) + self.means[action]
+        return self._get_obs(), reward, False, False, self._get_info()
 
     def render(self):
         pass
 
     def close(self):
-        pass
+        print('BanditEnv.close')
