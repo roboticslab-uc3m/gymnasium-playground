1eca470d380b35991c6a85a6d8031239b767a558
jgvictores <jcgvicto@gmail.com>
2023-01-07 16:54:56 +0100

    examples update render_mode

--- gridworld/examples/render.py
+++ gridworld/examples/render.py
@@ -3,7 +3,12 @@
 import gymnasium as gym
 import gymnasium_playground_gridworld
 
-env = gym.make('gymnasium_playground/GridWorld-v0', render_mode='human',
-               inFileStr='map1.csv', initX=2, initY=2, goalX=7, goalY=2)
+env = gym.make('gymnasium_playground/GridWorld-v0',
+               render_mode='human',  # "human", "text", None
+               inFileStr='map1.csv',
+               initX=2,
+               initY=2,
+               goalX=7,
+               goalY=2)
 env.reset()
 env.render()
