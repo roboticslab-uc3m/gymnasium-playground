8faceb43a0df971ebab58ecd3b529ac996fe0b00
FranJNaranjo <fjnaranjoc@gmail.com>
2023-12-14 20:11:12 +0100

    Add 5 episodes for testing after learning in ppo #9

--- grippers/examples/ppo-learning.py
+++ grippers/examples/ppo-learning.py
@@ -4,23 +4,27 @@ import gymnasium_playground_grippers
 from stable_baselines3 import PPO # DQN coming soon
 
 # check environnment
-env = gym.make('gymnasium_playground/Grippers-v0')
+env = gym.make('gymnasium_playground/Grippers-v0', random_init=True)
 
-model = PPO('MlpPolicy', env, verbose=1, tensorboard_log="./tensorboard/",learning_rate=0.01).learn(10000)
+model = PPO('MlpPolicy', env, verbose=1, tensorboard_log="./tensorboard/",learning_rate=0.01).learn(100000)
 
-obs, _ = env.reset()
-env.render()
+n_steps = 20
+n_epis = 5
 
-n_steps = 10
-
-for step in range(n_steps):
-    action, _ = model.predict(obs, deterministic=True)
-    print("Step {}".format(step + 1))
-    print("Action: ", action)
-    obs, reward, done, trucated, info = env.step(action)
+for epi in range(n_epis):
+    obs, _ = env.reset()
     env.render()
-    print("reward: ",reward )
 
-    if done:
-        print("Goal reached!", "reward=", reward)
-        break
+    for step in range(n_steps):
+        action, _ = model.predict(obs, deterministic=True)
+        print("Step {}".format(step + 1))
+        print("Action: ", action)
+        obs, reward, done, trucated, info = env.step(action)
+        env.render()
+        print("reward: ",reward )
+
+        if done:
+            print("*_*_*_*_*_*_*_*_*_*_*_*_")
+            print("Goal reached!", "reward=", reward)
+            print("*_*_*_*_*_*_*_*_*_*_*_*_")
+            break
